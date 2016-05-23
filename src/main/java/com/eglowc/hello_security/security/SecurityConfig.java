ef6ac5f43418a81517f9db78eb8f52e0f1de1806
eglowc <eglowc@gmail.com>
2016-05-23 11:36:32 +0900

    utf-8 적용한 주석 재작성

--- src/main/java/com/eglowc/hello_security/security/SecurityConfig.java
+++ src/main/java/com/eglowc/hello_security/security/SecurityConfig.java
@@ -12,9 +12,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     /**
      * <pre>
-     * ??? ???? Java Configuration configureGlobal ??? ??? ???? ???,
-     * ??? @EnableWebSecurity ? ???? ??? ???? {@link AuthenticationManagerBuilder}?
-     * ???? ??? ???.
+     * 스프링 시큐리티 Java Configuration configureGlobal 이라는 이름이 중요한건 아니다,
+     * 반드시 @EnableWebSecurity 를 붙인 클래스 내부에서 {@link AuthenticationManagerBuilder}를
+     * 구현해야 한다.
      * </pre>
      *
      * @param auth
@@ -29,8 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     /**
-     * {@link WebSecurityConfigurerAdapter}? ?????? ?? {@link #configure(HttpSecurity)} ???? ???? ?? ???,
-     * ??? ?? ??? ??? ????? ??? ????? ??? ???.
+     * {@link WebSecurityConfigurerAdapter}가 기본값으로 구현하고 있는 {@link #configure(HttpSecurity)} 설정 값이다.
      */
     /*
     protected void configure(HttpSecurity http) throws Exception {
@@ -49,11 +48,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .anyRequest()
-                .authenticated() // ??? ?????? ??? ????.
+                .authenticated() // 인증을 요구
                 .and()
-                .formLogin() // form ???
+                .formLogin() // form 인증
                 .loginProcessingUrl("/login")
-                .loginPage("/index.html") // ??? ??? ??
-                .permitAll(); // ?? ???? ??? ??
+                .loginPage("/index.html") // login page 지정
+                .permitAll(); // 모두 접근 가능
     }
 }
